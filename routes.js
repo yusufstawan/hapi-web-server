1eafc09b615a2589e035169e48b0a88b2540f570
yusufstawan <yusufsetiyawan45@gmail.com>
2023-02-08 06:33:14 +0700

    first commit

---
+++ routes.js
@@ -0,0 +1,16 @@
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Homepage'
+    }
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut'
+    }
+  }
+]
