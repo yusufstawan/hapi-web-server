5c9ce3ceb94dc4b426c52c9109c97a181dae9081
yusufstawan <yusufsetiyawan45@gmail.com>
2023-02-08 07:36:25 +0700

    path parameter

--- routes.js
+++ routes.js
@@ -3,16 +3,45 @@ const routes = [
     method: 'GET',
     path: '/',
     handler: (request, h) => {
-      return 'Homepage'
-    }
+      return 'Homepage';
+    },
   },
   {
     method: '*',
     path: '/',
     handler: (request, h) => {
-      return 'Halaman tidak dapat diakses dengan method tersebut'
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      return `Hello, ${name}!`;
     }
-  }
-]
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return 'Halaman tidak ditemukan';
+    },
+  },
+];
 
 module.exports = routes;
